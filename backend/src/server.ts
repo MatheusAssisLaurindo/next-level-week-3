b363a98e6780c85bdbef4aea8d8795f5c0dc76c8
Matheus Assis Laurindo <71344819+MatheusAssisLaurindo@users.noreply.github.com>
2020-10-18 13:53:26 -0300

    Enviando projeto final

---
+++ backend/src/server.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import 'express-async-errors';
+
+import './database/connection';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
+
+app.listen(3333);
+
+//Rota
+//Recurso= usuário
+
+//Métodos HTTP = GET, POST, PUT, DELETE
+//Parâmetros
+
+//GET= Buscar uma informação (Lista, Item)
+//POST = Criando uma informação
+//PUT = Editando uma informação
+//DELETE = Deletando uma informação
+
+//Query Params: http://localhost:3333/users?search=diego
+//Route Params: http://localhost:3333/users/1(Identificar um recurso)
+//Body: http://localhost:3333/users/1(Identificar um recurso)
